ac6ebaa0b080943783b1b79f00a7dd1193119a05
Andrejus <andrejusantoninovas@gmail.com>
2025-10-04 19:07:35 +0300

    Update update_lang.py

--- update_lang.py
+++ update_lang.py
@@ -54,9 +54,11 @@ def update_resume(repos_by_lang):
             if current_lang:
                 # Insert the new repo list for this language
                 for name, url, lang in repos_by_lang[current_lang]:
-                    print(name)
                     if name not in ["andrejusanto.github.io", "test-workflow"]:
-                        updated_lines.append(f"- [{name}]({url}) ({lang})\n")
+                        if name == "Turing-projektai":
+                            updated_lines.inset(0, f"- [{name}]({url}) ({lang})\n")
+                        else:
+                            updated_lines.append(f"- [{name}]({url}) ({lang})\n")
                 updated_lines.append("\n")
             continue
 
